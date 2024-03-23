1147b44343076ccad5476c58193a8c4ef99ae7d1
MartaGBayona <marta.g.bayona@outlook.es>
2024-03-23 22:51:10 +0100

    feat: create function to change header appearance on scroll

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import { CustomLink } from "../CustomLink/CustomLink"
 //import { useNavigate } from "react-router-dom";
 import "./Header.css"
 
 export const Header = () => {
+    const [headerScroll, setheaderScroll] = useState (false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const headerOnTop = window.scrollY || document.documentElement.headerOnTop;
+
+            if(headerOnTop > 0) {
+                setheaderScroll(true);
+            } else {
+                setheaderScroll(false);
+            }
+        }
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        };
+    }, []);
+
+
     //const navigate = useNavigate();
     const token = false;
     return (
-        <div className="headerDesign">
+        <div className={headerScroll ? "headerDesign scrolled" : "headerDesign"}>
             <CustomLink
                 title="Home"
                 destination="/"
