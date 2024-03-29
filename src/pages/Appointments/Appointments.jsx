3f46be5e2831c68f39c402fb7a724d57dfd8c1f7
MartaGBayona <marta.g.bayona@outlook.es>
2024-03-29 09:22:51 +0100

    update: modified function GetAppointment and UppdateAppointemnt. Can view user appointment but not modifiend

--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -1,73 +1,25 @@
-import "./Appointments.css"
+import "./Appointments.css";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { Header } from "../../common/Header/Header"
-import { validate } from "../../utils/functions";
+import { Header } from "../../common/Header/Header";
 import { GetAppointment, UpdateAppointment } from "../../services/apiCalls";
 import { CustomButton } from "../../common/customButton/customButton";
+import dayjs from "dayjs";
 import { CustomInput } from "../../common/Custominput/Custominput";
 
 export const Appointment = () => {
     
-    const navigate = useNavigate();
-    const dataAppointment = JSON.parse(localStorage.getItem("passport"));
-    const [tokenStorage, setTokenStorage] = useState(dataAppointment?.token);
+    const dataUser = JSON.parse(localStorage.getItem("passport"));
+    const [tokenStorage, setTokenStorage] = useState(dataUser?.token);
     const [loadedData, setLoadedData] = useState(false);
-    const [write, setWrite] = useState("disabled");
-
-    const [appointment, setAppointment] = useState({
-        id: "",  // Asegúrate de que appointment tenga un campo id
-        appointmentDate: "",
-        service: "",
-    });
-
-    const [appointmentError, setAppointmentError] = useState({
-        appointmentDateError: "",
-        serviceError: "",
-    });
-
-    const inputHandler = (e) => {
-        setAppointment((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
-    const checkError = (e) => {
-        const error = validate(e.target.name, e.target.value);
-        setAppointmentError((prevState) => ({
-            ...prevState,
-            [e.target.name + "Error"]: error
-        }))
-    }
-
+    const [appointments, setAppointments] = useState([]);
+    const [editIndex, setEditIndex] = useState(null);
 
     useEffect(() => {
         const getUserAppointment = async () => {
             try {
                 const fetched = await GetAppointment(tokenStorage);
-    
-                console.log("Respuesta del backend:", fetched);  // Depuración para verificar la respuesta del backend
-    
+                setAppointments(fetched.data);
                 setLoadedData(true);
-    
-                if (fetched && fetched.data && fetched.data.length > 0) {
-                    // Seleccionar la primera cita del array data por defecto
-                    const firstAppointment = fetched.data[0];
-    
-                    if (firstAppointment && firstAppointment.id) {
-                        setAppointment({
-                            ...appointment,
-                            id: firstAppointment.id,
-                            appointmentDate: firstAppointment.appointmentDate,
-                            service: firstAppointment.service.name,
-                        });
-                    } else {
-                        throw new Error("El ID del appointment es inválido");
-                    }
-                } else {
-                    throw new Error("No se pudo obtener el ID del appointment");
-                }
             } catch (error) {
                 console.log(error);
             }
@@ -78,71 +30,85 @@ export const Appointment = () => {
         }
     }, [tokenStorage, loadedData]);
     
-    const updateAppointment = async () => {
+    const updateAppointment = async (appointmentToUpdate) => {
         try {
-            // Verificar el valor del ID justo antes de actualizar
-            console.log("ID del appointment:", appointment.id);
+            console.log("ID del appointment:", appointmentToUpdate.id);
     
-            // Asegúrate de que appointment tenga un campo id válido
-            if (!appointment.id || appointment.id === "") {
+            if (!appointmentToUpdate.id || appointmentToUpdate.id === "") {
                 throw new Error("El ID del appointment es inválido");
             }
     
-            const fetched = await UpdateAppointment(tokenStorage, appointment);
+            const fetched = await UpdateAppointment(tokenStorage, appointmentToUpdate);
     
-            setAppointmentError((prevState) => ({
-                ...prevState,
-                appointmentDate: fetched.appointmentDate || prevState.appointmentDate,
-                service: fetched.service || prevState.service,
-            }));
+            console.log("Respuesta del backend:", fetched);
     
-            setWrite("disabled");
+            setLoadedData(false); 
+            setEditIndex(null);
         } catch (error) {
             console.log(error);
         }
+    };
+
+    const handleEdit = (index) => {
+        setEditIndex(index);
     };
 
     return (
         <>
             <Header />
             <div className="appointmentDesign">
-                {!loadedData ? (
-                    <div>CARGANDO</div>
-                ) : (
-                    <div className="appointmentDesign">
-
-                        <CustomInput
-                            className={`inputDesign ${appointmentError.appointmentDateError !== "" ? "inputDesignError" : ""
-                                }`}
-                            type={"appointmentDate"}
-                            placeholder={""}
-                            name={"appointmentDate"}
-                            disabled={write}
-                            value={appointment.appointmentDate || ""}
-                            onChangeFunction={(e) => inputHandler(e)}
-                            onBlurFunction={(e) => checkError(e)}
-                        />
-
-                        <CustomInput
-                            className={`inputDesign ${appointmentError.serviceError !== "" ? "inputDesignError" : ""
-                                }`}
-                            type={"service"}
-                            placeholder={""}
-                            name={"service"}
-                            disabled={write}
-                            value={appointment.service || ""}
-                            onChangeFunction={(e) => inputHandler(e)}
-                            onBlurFunction={(e) => checkError(e)}
-                        />
-
-                        <CustomButton
-                            className={write === "" ? "buttonDesign" : "buttonDesign"}
-                            title={write === "" ? "Confirmar" : "Editar"}
-                            functionEmit={write === "" ? updateAppointment : () => setWrite("")}
-                        />
-                    </div>
-                )}
+                <div>
+                    {
+                        loadedData && appointments.length > 0 
+                        ? (appointments.map((appointment, index) => {
+                             return (
+                                    <div key={appointment.id} className='appointStyle'>
+                                        <div>
+                                            Servicio: 
+                                            {editIndex === index ? (
+                                                <CustomInput
+                                                className="inputDesign "
+                                                    type="text"
+                                                    value={appointment.service.name}
+                                                    onChangeFunction={(e) => {
+                                                        const updatedAppointments = [...appointments];
+                                                        updatedAppointments[index].service.name = e.target.value;
+                                                        setAppointments(updatedAppointments);
+                                                    }}
+                                                />
+                                            ) : (
+                                                appointment.service.name
+                                            )}
+                                        </div>
+                                        <div>
+                                            Fecha: 
+                                            {editIndex === index ? (
+                                                <CustomInput
+                                                className="inputDesign "
+                                                    type="date"
+                                                    value={dayjs(appointment.appointment_date).format('YYYY-MM-DD')}
+                                                    onChangeFunction={(e) => {
+                                                        const updatedAppointments = [...appointments];
+                                                        updatedAppointments[index].appointment_date = dayjs(e.target.value).toISOString();
+                                                        setAppointments(updatedAppointments);
+                                                    }}
+                                                />
+                                            ) : (
+                                                dayjs(appointment.appointment_date).format('DD/MM/YYYY')
+                                            )}
+                                        </div>
+                                        <CustomButton
+                                            className="buttonDesign"
+                                            title={editIndex === index ? "Confirmar" : "Editar"}
+                                            functionEmit={editIndex === index ? () => updateAppointment(appointment) : () => handleEdit(index)}
+                                        />
+                                    </div>
+                                )
+                            })
+                        ): ("null")
+                    }
+                </div>
             </div>
         </>
     );
-}
+};
